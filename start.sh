29775757379d30edc826385aefb63c932286b9e7
SeanShe <sxy707@gmail.com>
2016-05-11 23:06:55 +0800

    update

--- start.sh
+++ start.sh
@@ -3,8 +3,9 @@ cowConfFile="~/.cow/rc"
 
 if [ ! -f "$cowConfFile" ]; then
 	echo listen = http://$LISTEN_IP:$LISTEN_PORT >> ~/.cow/rc
+	echo listen = http://$LISTEN_IP:$LISTEN_PORT >> /app/rc
 	echo proxy = ss://$SS_ENCRYPT_METHOD:$SS_PASSWORD@$SS_SERVER_IP:$SS_SERVER_PORT >> ~/.cow/rc
+	echo proxy = ss://$SS_ENCRYPT_METHOD:$SS_PASSWORD@$SS_SERVER_IP:$SS_SERVER_PORT >> /app/rc
 fi
-cp ~/.cow/rc /app/rc
 ./cow
 /bin/bash
