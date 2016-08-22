e20bef271d52777f1f802a43a40b7bfcd4f2c8f3
shexy <sxy707@gmail.com>
2016-08-22 15:55:41 +0800

    update

--- start.sh
+++ start.sh
@@ -2,4 +2,3 @@
 cat /dev/null > ~/.cow/rc
 echo listen = http://$LISTEN_IP:$LISTEN_PORT >> ~/.cow/rc
 echo proxy = ss://$SS_ENCRYPT_METHOD:$SS_PASSWORD@$SS_SERVER_IP:$SS_SERVER_PORT >> ~/.cow/rc
-/bin/bash
